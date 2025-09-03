cf2d6ea8b0bf5d66a4755ce57dc111189b417c61
Jcalvert22 <jacecalvert04@gmail.com>
2025-09-03 10:43:56 -0500

    Add files via upload

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,3 +1,20 @@
+document
+    .addEventListener("DOMContentLoaded", () => {
+        console.log("document succesfully loaded")
+        document.getElementById('btnClicky'); btnClicky.
+    addEventListener('click', btnClicky)
+    }
+    )
 
-console.log("we workin'")
-$(document).ready(alert("alert"))
+function btnClicky() {
+
+    document
+    console.log('in btnClicky event');
+    document.getElementById('btnClicky').style.transform = 'rotate(15deg)'
+    console.log('after rotation');
+}
+
+/*const btnClicky = document.getElementById('btnClicky'); btnClicky.
+    addEventListener('click', () => { // ...now do DOM stuff });
+        console.log("we workin'")
+        $(document).ready(alert("alert"))*/
